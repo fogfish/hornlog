b9d2e03e24b9f9697a8fd2ca21ca8b1a092bee79
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2016-01-31 22:06:48 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,77 @@
+# hornlog
+
+The library provide a primitives to define and evaluate horn clauses. The library implement a translation of logic program into executable code using pattern matching technique for evaluation.
+
+```
+u <- (p ^ q ^ ... ^ t)
+``` 
+
+## rules
+
+The rule is defined as implication clause `[p, q, ..., t]`. The rule is true if input pattern is matched. The library uses `'_'` to match any term (e.g. `[p, '_', ..., t]`).   
+
+There are three type of rules: headless, identity and augmented. The _headless_ rule return input subject if pattern is matched `hornlog:rule([p, q, ..., t])`. The _identity_ rule curry term to identity function and return this value with curried value if pattern is matched `hornlog:rule(hornlog:id(u), [p, q, ..., t])`. The _augmented_ rule curries terms to any external function, which is called if pattern is matched `hornlog:rule(hornlog:head(fun myapp:u/2, [u]), [p, q, ..., t])`.
+
+## compile and evaluate 
+
+The rule compiler transforms set of rules into executable program, which is loaded and manged by Erlang code server `hornlog:c(my_rule_test, [...])`.
+  
+
+The rules evaluation process takes two arguments: pattern (feature vector) and subject. The feature vector is matched against previously defined pattern. The subject is feed to head function if pattern matches the rule `hornlog:q(my_rule_test, [p, q, ..., t], {...})`.
+ 
+## example
+
+Classical play tennis example from Machine Learning books. The feature vector consists of outlook, temperature, humidity, wind.
+
+```
+hornlog:c(play_tennis, [
+   hornlog:rule(hornlog:id(true),  [overcast, hot,  high, weak]),
+   hornlog:rule(hornlog:id(true),  [overcast, cool, low,  strong]),
+   hornlog:rule(hornlog:id(true),  [overcast, mild, high, strong]),
+   hornlog:rule(hornlog:id(true),  [overcast, hot,  low,  weak]),
+
+   hornlog:rule(hornlog:id(false), [rainy,    cool, low,  strong]),
+   hornlog:rule(hornlog:id(false), [rainy,    mild, high, strong]),
+   hornlog:rule(hornlog:id(true),  [rainy,    mild, high, weak]),
+   hornlog:rule(hornlog:id(true),  [rainy,    cool, low,  weak]),
+   hornlog:rule(hornlog:id(true),  [rainy,    mild, low,  weak]),
+
+   hornlog:rule(hornlog:id(false), [sunny,    hot,  high, weak]),
+   hornlog:rule(hornlog:id(false), [sunny,    hot,  high, strong]),
+   hornlog:rule(hornlog:id(false), [sunny,    mild, high, weak]),
+   hornlog:rule(hornlog:id(true),  [sunny,    cool, low,  weak]),
+   hornlog:rule(hornlog:id(true),  [sunny,    mild, low,  strong])
+]).
+
+
+hornlog:q(play_tennis, [rainy, cool, low, weak], me).
+```
+
+
+
+
+hornlog:c(hornlog_test, [
+   hornlog:rule(hornlog:id(1), [a])
+]).
+
+
+
+hornlog:c(hornlog_test, [
+   hornlog:rule([a, b, c]),
+   hornlog:rule([a]),
+   hornlog:rule([a, b]),
+   hornlog:rule([a, '_', c]),
+   hornlog:rule([b, 1])
+]).
+
+hornlog:q(hornlog_test, [c], this).
+
+hornlog:c(hornlog_test, [
+   hornlog:rule([a, b, c]),
+   hornlog:rule([a]),
+   hornlog:rule([a, b]),
+   hornlog:rule([a, '_', c]),
+   hornlog:rule([b, 10])
+]).
+
+
---
+++ src/hornlog.erl
@@ -0,0 +1,179 @@
+%%
+%%   Copyright (c) 2016, Dmitry Kolesnikov
+%%   All Rights Reserved.
+%%
+%%   Licensed under the Apache License, Version 2.0 (the "License");
+%%   you may not use this file except in compliance with the License.
+%%   You may obtain a copy of the License at
+%%
+%%       http://www.apache.org/licenses/LICENSE-2.0
+%%
+%%   Unless required by applicable law or agreed to in writing, software
+%%   distributed under the License is distributed on an "AS IS" BASIS,
+%%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%   See the License for the specific language governing permissions and
+%%   limitations under the License.
+%%
+-module(hornlog).
+
+-export([
+   head/2, id/2, id/1,
+   rule/2, 
+   rule/1, 
+   c/2, 
+   q/3
+]).
+
+%%
+%% data types
+-type head()    :: _.
+-type pattern() :: [_].
+-type rule()    :: _.
+
+
+%%
+%% define head of rule by curring argument(s) to the function
+-spec head(function(), [_]) -> head().
+
+head(Fun, List) ->
+   curry(
+      lens:get(flens(), lens:pair(module), Fun),
+      lens:get(flens(), lens:pair(name),   Fun),
+      List
+   ).
+
+%%
+%% identity head, curry argument to identity function
+-spec id(_) -> head().
+
+id(X) ->
+   head(fun hornlog:id/2, [X]).
+
+id(X, Y) ->
+   {X, Y}.
+
+
+%%
+%% define new rule
+-spec rule(head(), pattern()) -> rule().
+-spec rule(pattern()) -> rule().
+
+rule(Head, Pattern) ->
+   Nary = length(Pattern),
+   Wary = length([X || X <- Pattern, X =/= '_']),
+   % estimate it arity and weighted arity (number of 'explicit' term match)
+   {{Nary, Wary}, Pattern, Head}.   
+
+rule(Pattern) ->
+   %% bind identity function to rule head 
+   rule({var, 0, 'X'}, Pattern).
+
+
+%%
+%% compile rules and load its executable code to code server
+-spec c(atom(), [rule()]) -> {module, atom()}.
+
+c(Id, Rules) ->
+   {ok, Id, Bin} = compile:forms(unit(Id, Rules), []),
+   code:load_binary(Id, undefined, Bin).
+
+%%
+%% evaluate rules, throws exception if evaluation is failed
+-spec q(atom(), pattern(), _) -> _.
+
+q(Id, Pattern, X) ->
+   erlang:apply(Id, q, [X | Pattern]).
+
+%%%----------------------------------------------------------------------------   
+%%%
+%%% private
+%%%
+%%%----------------------------------------------------------------------------   
+
+%%
+%% transform rules to compilable form (code unit)  
+unit(Id, Rules) ->
+   [  
+      {attribute, 0, module, Id}
+     ,{attribute, 0, compile, export_all}
+     |rule_match_function(
+         rule_roll_up_arity(
+            rule_sort_by_arity(Rules)
+         )
+      )
+   ] ++ [{eof, 0}].
+
+
+%%
+%% transform rule to match function(s)
+rule_match_function([Head | Tail]) ->
+   [pattern_match(Head) | rule_match_function(Tail)];
+
+rule_match_function([]) ->
+   [].
+
+pattern_match([{{Nary, _}, _, _} | _] = Rules) ->
+   {function, 0, q, Nary + 1,
+      lists:map(
+         fun({_, Pattern, Head}) ->
+            match(Pattern, Head)
+         end,
+         Rules
+      )
+   }.
+
+
+%%
+%% roll-up rules with same arity to single code block
+rule_roll_up_arity([]) ->
+   [];
+rule_roll_up_arity(Rules) ->
+   N = lens:get(lens:hd(), lens:t1(), lens:t1(), Rules),
+   {Head, Tail} = lists:splitwith(
+      fun(X) ->
+         N =:= lens:get(lens:t1(), lens:t1(), X)
+      end,
+      Rules
+   ),
+   [Head | rule_roll_up_arity(Tail)].
+
+
+%%
+%% sort rules by it arity
+rule_sort_by_arity(Rules) ->
+   lists:sort(
+      fun(A, B) ->
+         erlang:element(1, A) >= erlang:element(1, B)
+      end,
+      Rules
+   ).
+
+%% 
+%% read only lens to focus on function meta-data
+flens() ->
+   fun(Fun, Function) ->
+      lens:fmap(fun(_) -> Function end, Fun( erlang:fun_info(Function) ))
+   end.
+
+%%
+%% curry external function
+curry(Mod, Fun, List) ->
+   {call, 0,
+      {remote, 0, {atom, 0, Mod}, {atom, 0, Fun}},
+      [erl_parse:abstract(X) || X <- List] ++ [{var, 0, 'X'}]
+   }.
+
+%%
+%% return pattern match clause
+match(Pattern, Body) ->
+   {clause, 0,  
+      [{var, 0, 'X'}|lists:map(fun match/1, Pattern)], 
+      [],
+      [Body]
+   }.
+
+match('_') ->
+   {var, 0, '_'};
+match(X)   ->
+   erl_parse:abstract(X).
+
